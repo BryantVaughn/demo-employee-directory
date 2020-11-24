b259f45428265355b4799189a65d0fe39b520491
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-11-23 17:25:35 -0700

    Add styles for employee cards and finalize mobile view

--- js/users.js
+++ js/users.js
@@ -36,15 +36,20 @@ function generateUserCards(users) {
 
 		// Create elements that make up each employee card
 		const img = createElement('img', 'profile-img');
-		img.src = picture.thumbnail;
+		img.src = picture.medium;
 		img.alt = `Picture of ${fullName}`;
+		const textDiv = createElement('div', 'employee-text');
 		const nameH3 = createElement('h3', 'name', `${fullName}`);
 		const emailP = createElement('p', 'email', email);
 		const locationP = createElement('p', 'location', location.city);
 
 		// Create array of elements to append to div
-		const childItems = [img, nameH3, emailP, locationP];
+		const textItems = [nameH3, emailP, locationP];
+		appendItems(textDiv, textItems);
+
+		const childItems = [img, textDiv];
 		appendItems(card, childItems);
+
 		appendItems(employees, [card]);
 	});
 }
